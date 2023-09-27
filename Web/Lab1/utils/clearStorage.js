710754105d066730bf5fe5fb273ac3e7ecb592c1
Black Rider <42sdn@mail.ru>
2023-09-27 23:38:00 +0700

    Delete Web/Lab1 directory

--- Web/Lab1/utils/clearStorage.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.danger-zone').addEventListener('click', function (){
-    const userConfirmation = confirm('Are you sure you want to clear table data?');
-    if (userConfirmation){
-        localStorage.clear();
-        location.reload();
-    }
-})
--- Web/Lab1/utils/switchThemes.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function (){
-        const themeSwitch = document.getElementById("themeSwitch");
-        themeSwitch.addEventListener("click", function (event){
-            const theme = event.target.checked ? "dark" : "light";
-            document.documentElement.setAttribute("data-theme", theme);
-            sessionStorage.setItem("currTheme", theme);
-            drawCoordsPlane(0);
-        });
-})
